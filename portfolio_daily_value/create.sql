f1171192e127674c16bb27158f372264b89c417c
CThaw90 <cthaw@mediaocean.com>
2025-03-20 21:40:17 -0400

    Renamed portfolio_daily_value table to portfolio_balance_daily_history and made it a child of the portfolio_balance table

--- portfolio_daily_value/create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE portfolio_daily_value (
-	id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	portfolio_id INTEGER UNSIGNED NOT NULL,
-	cash INTEGER UNSIGNED NOT NULL,
-	equity INTEGER UNSIGNED NOT NULL,
-	event_date DATE NOT NULL,
-	CONSTRAINT portfolio_id_fk FOREIGN KEY (portfolio_id) REFERENCES portfolio (id)
-);
