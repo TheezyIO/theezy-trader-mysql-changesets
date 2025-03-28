a1cf0441b7ea623d90f8003045418b57070cec55
Josue Resilien <joshrez20@gmail.com>
2025-03-28 17:56:50 -0400

    Chnaged the statements for account_balance testing

--- account_balance/modify_fk_and_index_names.sql
+++ account_balance/modify_fk_and_index_names.sql
@@ -1,8 +1,8 @@
 ALTER TABLE account_balance
-DROP FOREIGN KEY account_balance_ibfk_1,
-ADD CONSTRAINT user_id_fk FOREIGN KEY (user_id) 
-REFERENCES user (id) ON DELETE CASCADE;
+DROP FOREIGN KEY account_balance_ibfk_1;
 
 ALTER TABLE account_balance
 DROP INDEX user_id,
-ADD INDEX user_id_idx (user_id);
+ADD INDEX user_id_idx (user_id),
+ADD CONSTRAINT user_id_fk FOREIGN KEY (user_id) 
+REFERENCES user (id) ON DELETE CASCADE;
